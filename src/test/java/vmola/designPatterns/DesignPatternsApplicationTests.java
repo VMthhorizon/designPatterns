db6038bfbe934112108e9ffba485cd78c290e3ed
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-07-15 15:38:38 +0200

    Initial commit

---
+++ src/test/java/vmola/designPatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package vmola.designPatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
